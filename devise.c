e544fb948a094f47f7fc35f0dd4223f47caf7688
Alvin Zhu <alvinzhu33@yahoo.com>
2016-10-26 15:31:57 -0400

    successfully found random ints

--- devise.c
+++ devise.c
@@ -3,6 +3,18 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+void randInt(){
+  int table[10];
+
+  int i=0;
+  int random = open("/dev/random", O_RDONLY);
+  for(i; i<10; i++){
+    read(random, &table[i], 4);
+    printf("index %d: %d\n", i, table[i]);
+  }
+}
+
 int main(){
+  randInt();
   return 0;
 }
